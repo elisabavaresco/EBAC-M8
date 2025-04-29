bdade5def3cd85713b0f4f92de0f4b272dc4485c
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-04-29 15:24:55 -0300

    estudo lambda

--- aula8-2.py
+++ aula8-2.py
@@ -1,3 +1,5 @@
+# Introdução ao tratamento de dados
+
 import pandas as pd
 
 df = pd.read_csv('clientes.csv')
@@ -5,10 +7,13 @@ df = pd.read_csv('clientes.csv')
 # Verificar os primeiros registros
 print(df.head().to_string())
 
+# Verificar os últimos registros
+print(df.tail().to_string())
+
 # Verificar qtd de linhas e colunas
 print('Qtd: ', df.shape)
 
-# Verificar tipos de dados
+# Verificar tipos de dados (se é int, flot ou object)
 print('Tipagem: \n', df.dtypes)
 
 # Checar valores nulos
