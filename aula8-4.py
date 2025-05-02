f00115293e57ce3ff0e50d04c86ccc954e28170e
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-05-02 15:03:53 -0300

    anotações aula 8.4, identificando outliers com z-score

--- aula8-4.py
+++ aula8-4.py
@@ -1,2 +1,18 @@
 import pandas as pd
 from scipy import stats
+
+pd.set_option('display.width', None)
+
+df = pd.read_csv('clientes.csv')
+
+df_filtro_basico = df[df['idade'] > 99]
+
+print('Filtro básico \n', df_filtro_basico[['nome', 'idade']])
+
+# Identificar outliers com Z-score
+z_scores = stats.zscore(df['idade'].dropna())
+outliers_z = df[z_scores >= 3]
+print("Outliers pelo Z-score: \n", outliers_z)
+
+# Filtrar outliers com Z-score
+df_zscore = df[(stats.zscore(df['idade']) < 3 )]
